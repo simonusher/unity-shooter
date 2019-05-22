7c5293752747890d5db4e3c3bdb37954242ccc2b
simon_wozniak <wozniaks97@gmail.com>
2019-05-22 19:19:17 +0200

    add nav mesh agent

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    [SerializeField] private Transform goal = null;
+    private NavMeshAgent agent = null;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+    }
+
+    private void Update()
+    {
+        agent.destination = goal.position;
+    }
+}
